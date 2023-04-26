9c3f698cc681e0efa09be5bb667c9ead5a02fcc7
Om <mr.omchaudhary007@gmail.com>
2023-04-26 12:11:30 +0530

    number of 1's

---
+++ Number of 1's in binary number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int number_of_ones(int n);
+int main(){
+	int n,ans;
+	cout<<"Enter the number: ";
+	cin>>n;
+	ans=number_of_ones(n);
+	cout<<"Number of 1's in "<<n<<" is = "<<ans<<endl;
+	return 0;
+}
+int number_of_ones(int n){
+	int count=0;
+	while(n!=0){
+		if(n&1){
+		count++;
+	    }
+		n=n>>1;
+	}
+	return count;
+}
