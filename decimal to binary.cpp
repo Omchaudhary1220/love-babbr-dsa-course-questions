f7056d180679a7d632410bea946c4668a7e0788f
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-05-08 12:09:02 +0530

    Update decimal to binary.cpp

--- decimal to binary.cpp
+++ decimal to binary.cpp
@@ -1,17 +1,20 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-int decimal_to_bianry(int n);
+long long int decimal_to_binary(long long int  n);
 int main(){
-	int n,ans;
+	long long int n,ans;
 	cout<<"Enter the decimal number: ";
 	cin>>n;
-	ans=decimal_to_bianry(n);
+	ans=decimal_to_binary(n);
 	cout<<"Binary of "<<n<<" is "<<ans<<endl;
 	return 0;
 }
-int decimal_to_bianry(int n){
-	int bit=0,i=0;
+long long int decimal_to_binary(long long int n){
+	long long int bit=0,i=0;
+	if(n<0){
+	n=pow(2,16)+n;
+	}
 	while(n!=0){
 		bit=((n&1)*pow(10,i))+bit;
 		n=n>>1;
