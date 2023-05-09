78073f903a4d6a4fb1452daa56bdebf36e11024f
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-05-09 13:18:37 +0530

    Create pair_sum.cpp

---
+++ pair_sum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void pair_sum(int arr[],int n,int k){
+    int i,j;
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]+arr[j]==k){
+                cout<<i<<" "<<j<<endl;
+            }
+        }
+    }
+    
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(int);
+    int k=5;
+    pair_sum(arr,n,k);
+    return 0;
+}
