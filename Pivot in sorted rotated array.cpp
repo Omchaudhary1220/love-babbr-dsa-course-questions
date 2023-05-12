9c56a6fd0e4263597fb21401ff0ebe26417535e0
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-05-12 16:23:49 +0530

    Create Pivot in sorted rotated array.cpp
    A pivot element is an element which is smaller than all other element and given array is roatated sorted array.

---
+++ Pivot in sorted rotated array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int pivotindex(int arr[],int n){
+    int l=0,h=n-1;
+    int mid;
+    mid=l+(h-l)/2;
+    while(l<h){
+        if(arr[mid]>=arr[0]){
+            l=mid+1;
+        }
+        else{
+            h=mid;
+        }
+      mid=l+(h-l)/2;
+    }
+    return mid;
+}
+int main(){
+    int arr[]={3,8,10,17,1};
+    int n=sizeof(arr)/sizeof(int);
+    int index=pivotindex(arr,n);
+    cout<<"ans is "<<index<<endl;
+    return 0;
+}
