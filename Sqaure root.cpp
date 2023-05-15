22e6f092e9ad906977d879d86069f9fdb51f63fa
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-05-15 20:20:13 +0530

    Update Sqaure root.cpp

--- Sqaure root.cpp
+++ Sqaure root.cpp
@@ -13,6 +13,26 @@ int SqureRoot(int n){
 		}
 	}
 }
+//optimal solution using binary search O(logn)
+int Sqaureroot(int n){
+   int s=0,e=n;
+   int mid,ans=-1,square;
+   while(s<=e){
+       mid=(s+e)/2;
+       square=mid*mid;
+       if(square==n){
+           return mid;
+       }
+       else if(square<n){
+           ans=mid;
+           s=mid+1;
+       }
+       else{
+           e=mid-1;
+       }
+   }
+   return ans;
+}
 int main()
 {
     int n;
