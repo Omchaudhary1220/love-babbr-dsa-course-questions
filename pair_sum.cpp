abee9e4544a0f232dc60ce125d3806ece6edbf28
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-05-21 15:55:29 +0530

    Update pair_sum.cpp

--- pair_sum.cpp
+++ pair_sum.cpp
@@ -1,6 +1,24 @@
 Link:- https://www.codingninjas.com/codestudio/problems/pair-sum_1171154
 #include<iostream>
 using namespace std;
+//brute-force solution
+int pairSum(vector<int> &arr, int n, int target){
+	int i,j,count=0;
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]+arr[j]==target){
+                count++;
+            }
+        }
+    }
+	if(count==0){
+        return -1;
+    }
+    else{
+        return count;
+    }
+}
+//optimise solution
 int pair_sum(int arr[],int n,int k){
     int i=0,j=n-1,count=0;
     while(i<j){
