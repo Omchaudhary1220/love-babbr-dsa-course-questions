70b5cc115f3b8c0a5ff1eba441f3fb318ae2e90c
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-05-21 15:39:14 +0530

    Update pair_sum.cpp

--- pair_sum.cpp
+++ pair_sum.cpp
@@ -1,21 +1,31 @@
 Link:- https://www.codingninjas.com/codestudio/problems/pair-sum_1171154
 #include<iostream>
 using namespace std;
-void pair_sum(int arr[],int n,int k){
-    int i,j;
-    for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            if(arr[i]+arr[j]==k){
-                cout<<i<<" "<<j<<endl;
-            }
+int pair_sum(int arr[],int n,int k){
+    int i=0,j=n-1,count=0;
+    while(i<j){
+        if(arr[i]+arr[j]==k){
+            count++;
+            i++;
+            j--;
+        }
+        else if(arr[i]+arr[j]<k){
+            i++;
+        }
+        else{
+            j--;
         }
     }
-    
+    if(count==0){
+        return -1;
+    }
+    else
+    return count;
 }
 int main(){
     int arr[]={1,2,3,4,5};
     int n=sizeof(arr)/sizeof(int);
     int k=5;
-    pair_sum(arr,n,k);
+    cout<<"count is "<<pair_sum(arr,n,k);
     return 0;
 }
