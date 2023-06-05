7b56677aed19baec5b16449008896819cc7eae9c
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-06-05 22:07:03 +0530

    Create Is_palidrome_string.cpp

---
+++ Is_palidrome_string.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(char* p,char* q){
+    char temp=*p;
+    *p=*q;
+    *q=temp;
+}
+//using extra space
+bool pal(string str){
+    int i=0,j=str.size()-1;
+    while(i<j){
+        if(str[i++]!=str[j--]){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+    return 0;
+}
+
+//without using extra space 
+bool palidrome(string str){
+    string s=str;
+    reverse(s.begin(),s.end());
+    if(s==str){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    
+}
+int main(){
+    string str;
+    cout<<"Enter the string: ";
+    cin>>str;
+    cout<<"is palidrome: "<<pal(str)<<endl;
+    return 0;
+}
