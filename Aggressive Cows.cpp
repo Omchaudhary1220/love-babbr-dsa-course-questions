711535a70ea2cd3fe39b9e29c5028bd2469c99a7
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-06-10 17:48:46 +0530

    Update Aggressive Cows.cpp

--- Aggressive Cows.cpp
+++ Aggressive Cows.cpp
@@ -1,4 +1,4 @@
-Link:- https://www.codingninjas.com/codestudio/problems/aggressive-cows_1082559?source=youtube&campaign=love_babbar_codestudio2&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio2
+Link:-https://www.codingninjas.com/codestudio/problems/aggressive-cows_1082559?source=youtube&campaign=love_babbar_codestudio2&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio2
 #include<bits/stdc++.h>
 using namespace std;
 int aggressiveCows(vector<int> &stalls, int k);
