a48802f3ebaa1220621e36182e9bfcc70bd7d13e
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-07-05 15:32:38 +0530

    Update Merge2_sorted_array.cpp

--- Merge2_sorted_array.cpp
+++ Merge2_sorted_array.cpp
@@ -1,4 +1,7 @@
-Link:- https://leetcode.com/problems/merge-sorted-array/description/
+// Link:- https://leetcode.com/problems/merge-sorted-array/description/
+// for this question we have to merge both array and store into first array.
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -15,3 +18,45 @@ public:
         }
     }
 };
+//using an extra vector
+vector<int> merge(vector<int>arr,int n1,vector<int>arr1,int n2){
+    int i=0,j=0;
+    vector<int>ans;
+    while(i<n1&&j<n2){
+        if(arr[i]<arr1[j]){
+            ans.push_back(arr[i]);
+            i++;
+        }
+        else if(arr[i]>arr1[j]){
+            ans.push_back(arr1[j]);
+            j++;
+        }
+        else{
+            ans.push_back(arr[i]);
+            ans.push_back(arr1[j]);
+            i++;
+            j++;
+        }
+    }
+    while(i<n1){
+        ans.push_back(arr[i]);
+        i++;
+    }
+    while(j<n2){
+        ans.push_back(arr1[j]);
+        j++;
+    }
+    return ans;
+}
+int main(){
+    vector<int>arr={2,4,6,8,10};
+    vector<int>arr1={10,20,30,40};
+    int n1=arr.size();
+    int n2=arr1.size();
+    vector<int>ans;
+    ans=merge(arr,n1,arr1,n2);
+    for(auto i:ans){
+        cout<<i<<" ";
+    }
+    return 0;
+}
